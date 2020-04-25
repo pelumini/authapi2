5e43113e3b505e6728ce5c0ba5ceee0a09d6ab4d
pelumini <pelumini@gmail.com>
2020-04-25 04:59:51 +0100

    more changes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const { mongourl, port } = require('./config/keys');
+
+// Connect to MongoDB
+mongoose.connect(mongourl, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const db = mongoose.connection
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+// Essential Middlewares
+app.use(cookieParser());
+app.use(express.json());
 
 app.listen(port || 5000, () => console.log(`Express Server is running on port ${port}`));
 
